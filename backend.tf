856be008b515eb5152cad404ddeaef6c50312e5b
Ketone16 <122990924+Ketone16@users.noreply.github.com>
2023-04-12 23:50:51 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,5 +4,7 @@ terraform {
     key = "main"
     region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table"
+    access_key = "AKIAYP5CFF43RVQ3RA4N"
+    secret_key = "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"
   }
 }
