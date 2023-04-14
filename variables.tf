53d072099ead5457694ed21309445c8afead1f51
Hulkbuster <abc@gmail.com>
2023-04-14 17:02:00 +0530

    changes done in variable

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "myJune2021Key" 
+    default     =  "myJune2022Key" 
 }
 
 variable "instance_type" { 
@@ -24,7 +24,7 @@ variable "tag_name" {
 } 
 variable "ami_id" { 
     description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-07ffb2f4d65357b42" 
+    default     = "ami-06e46074ae430fba6" 
 }
 variable "versioning" {
     type        = bool
